2804e8be4dd31b013d5d2a86f7e2a05cc77a6628
9oodam <godamcity98@gmail.com>
2024-12-09 15:41:32 +0900

    test

--- yolo_test.py
+++ yolo_test.py
@@ -1,21 +1,15 @@
 from flask import Flask, request, jsonify
-import logging
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO)
-
 @app.route('/test', methods=['POST'])
 def test() :
-    app.logger.info('Request received: %s', request.json)
-    # data = request.get_json()
-    return {"message": "Test successful"}
-
-    # return jsonify({
-    #     "status": "success",
-    #     "message": "test succeeded",
-    #     "data": data
-    # })
+    data = request.get_json()
+    return jsonify({
+        "status": "success",
+        "message": "test succeeded",
+        "data": data
+    })
 if __name__ == '__main__':
     print("server start on http://0.0.0.0:9001")
     app.run(host='0.0.0.0', port=9001)
